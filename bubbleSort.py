aba894dfeb654973e59c006b0f8100ab21424ea6
Kartikey Kakaria <kartikeykakaria07@gmail.com>
2025-03-09 18:26:38 +0530

    file9

---
+++ bubbleSort.py
@@ -0,0 +1,9 @@
+# Program to implement bubble sort
+numbers = [64, 34, 25, 12, 22, 11, 90]
+
+for i in range(len(numbers)):
+    for j in range(0, len(numbers) - i - 1):
+        if numbers[j] > numbers[j + 1]:
+            numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+
+print("Sorted list is:", numbers)
